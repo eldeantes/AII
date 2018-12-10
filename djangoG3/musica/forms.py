f0895b18fc4649931f531b47c0d2df7ca9d512f7
Angel <angelmarmolfernandez@gmail.com>
2018-12-10 12:59:14 +0100

    asda

---
+++ djangoG3/musica/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+class DiscograficaForm(forms.Form):
+    nombre = forms.CharField(label='Nombre', max_length=100)
+    pais = forms.CharField(label='Pais', max_length=100)
+    
+class UsuarioForm(forms.Form):
+    nombre = forms.CharField(label='Nombre', max_length=100)
+    username = forms.CharField(label='Nombre de usuario', max_length=100)
+    password = forms.CharField(label='Password', max_length=100)
+    apellidos = models.CharField(label='Apellidos', max_length=100)
+    email = models.EmailField(label='email', max_length=70, null=True, blank=True, unique=True)
+    
+class AstistaForm(forms.Form):
+    nombre = forms.CharField(label='Nombre', max_length=100)
+    pais = forms.CharField(label='Pais', max_length=100)
+    
--- djangoG3/musica/views.py
+++ djangoG3/musica/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'peliculas/index.html', context)
+
+def newDiscografica(request):
+    return render(request, 'peliculas/newDiscografica.html', context)
+
