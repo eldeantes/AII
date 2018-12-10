a610ab435faabd0431672ba98f13a213e0a96280
Angel <angelmarmolfernandez@gmail.com>
2018-12-10 12:48:25 +0100

    ey

--- djangoG3/musica/admin.py
+++ djangoG3/musica/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Usuario)
+admin.site.register(Discografica)
+admin.site.register(Artista)
